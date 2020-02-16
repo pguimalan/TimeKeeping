220f4667bf58488ea855ea4434eb8922f905892b
pguimalan <pguimalan@gmail.com>
2020-02-17 00:03:23 +0800

    no message

--- TimeKeeping.Models/DeparmentModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TimeKeeping.Models
-{
-    public class DepartmentModel
-    {
-        public int DepartmentId { get; set; }
-        public string DepartmentName { get; set; }
-    }
-}
